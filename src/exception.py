9a893cd0a7a0684ab56a41de72c42d128de02739
latha <lathakh2018@gmail.com>
2025-02-21 18:10:16 +0530

    added excpetion and logging

--- src/exception.py
+++ src/exception.py
@@ -1,3 +1,4 @@
+
 # custom exception
 
 import sys 
@@ -5,14 +6,14 @@ from src.logger import logging
 
 def error_message_details(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filenane
-    error_message="error occured in the python script name [{0}] line number [{1}] error message[{2}]".format(file_name,exc_tb.tc_lineno,str(error))
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message="error occured in the python script name [{0}] line number [{1}] error message[{2}]".format(file_name,exc_tb.tb_lineno,str(error))
     return error_message
 
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
-        self.error_meassage=error_message_details(error_message,error_detail=error_detail)
+        self.error_message=error_message_details(error_message,error_detail)
 
     def __str__(self):
         return self.error_message        
@@ -20,7 +21,7 @@ class CustomException(Exception):
 
 if __name__ == '__main__':
     try:
-        a=10
+        a=1/0
     except Exception as e:
         logging.info("divide by zero error")
         raise CustomException(e,sys)       
--- src/logger.py
+++ src/logger.py
@@ -3,18 +3,21 @@ import os
 from datetime import datetime
 
 
-#file name got cretaed with log
-LOG_FILE = f"{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd(), 'logs',LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
+#create logs directory
+LOG_DIR =os.path.join(os.getcwd(), 'logs') 
+os.makedirs(LOG_DIR,exist_ok=True)
 
-LOG_FILE_PATH=os.path.join(logs_path, 'LOG_FILE')
+#create log file with timestamp
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE_PATH=os.path.join(LOG_DIR, LOG_FILE)
 
-# overwrite the error message  with latest
-logging.basicConfig(filename=LOG_FILE_PATH,format="[%(asctime)s] %(levelname)s %(message)s",level=logging.INFO)
+# configuring logging
+logging.basicConfig(filename=LOG_FILE_PATH,
+                    format="[%(asctime)s ] %(lineno)d %(name)s - (levelname)s - %(message)s",
+                    level=logging.INFO)
 
 if __name__=="__main__":
-    logging.info("logging has started")
+    logging.info("logging has successdully setup")
+    logging.error("this is the testing error")
 
    
-
